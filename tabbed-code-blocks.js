e54aab8857963137fcde0deac87b7163f65b933f
Rob Moss <robm.dev@gmail.com>
2022-10-03 11:55:09 +1100

    Only convert top-level code blocks into tabs
    Rust code blocks may use a nested pre element to contain action buttons
    (e.g., "Copy to clipboard" and "Run this code") and these nested pre
    elements should therefore be ignored. We use the '>' combinator to only
    select the top-level pre elements.

--- tabbed-code-blocks.js
+++ tabbed-code-blocks.js
@@ -33,7 +33,8 @@ function addTabsToGroup(group) {
     group.prepend(tab_bar);
 
     // Add a tab for each code block, and hide all but the first block.
-    const pre_elements = group.querySelectorAll("pre");
+    // Note the use of the '>' combinator to only select top-level blocks.
+    const pre_elements = group.querySelectorAll(":scope > pre");
     pre_elements.forEach((pre_elt, ix) => {
         const tab_elt = addTabForCodeBlock(tab_bar, pre_elements, pre_elt);
         if (ix > 0) {
