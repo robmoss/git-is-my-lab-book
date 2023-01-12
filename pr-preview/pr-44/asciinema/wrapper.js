827b8f143dc4228ffb6ba7b1b79430f80a66abe7
robmoss <robmoss@users.noreply.github.com>
2023-01-12 02:08:27 +0000

    Deploy preview for PR 44 🛫

--- pr-preview/pr-44/asciinema/wrapper.js
+++ pr-preview/pr-44/asciinema/wrapper.js
@@ -30,7 +30,7 @@ function addAsciinemaPlayer(cast_file, container_id) {
     const player = AsciinemaPlayer.create(
         cast_file,
         document.getElementById(container_id),
-        { poster: 'npt:0:1', theme: 'dracula' });
+        { poster: 'npt:0:0.1', theme: 'dracula' });
 
     // Find all elements that seek to specific times in this video.
     const selector = '[data-video="' + container_id + '"][data-seek-to]'
